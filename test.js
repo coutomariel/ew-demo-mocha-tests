ce3d4ab8a9cdeacf771bb44da05b69e005ed82cd
Mariel <coutomariel@gmail.com>
2020-08-18 22:22:07 -0300

    First commit

---
+++ test.js
@@ -0,0 +1,54 @@
+// Funcionalidade nativa do node 
+const assert = require('assert');
+// assert.ok(true);
+
+const { obterPessoas } = require('./service');
+
+// Utilização do nock para mockar insumos do testes
+const nock = require('nock');
+
+// Suite de testes
+describe('Star Wars tests', function () {
+    this.beforeAll(() => {
+        const response = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: [
+                {
+                    name: 'R2-D2',
+                    height: '96',
+                    mass: '32',
+                    hair_color: 'n/a',
+                    skin_color: 'white, blue',
+                    eye_color: 'red',
+                    birth_year: '33BBY',
+                    gender: 'n/a',
+                    homeworld: 'http://swapi.dev/api/planets/8/',
+                    vehicles: [],
+                    starships: [],
+                    created: '2014-12-10T15:11:50.376000Z',
+                    edited: '2014-12-20T21:17:50.311000Z',
+                    url: 'http://swapi.dev/api/people/3/'
+                }
+            ]
+        }
+
+        nock('https://swapi.dev/api/people')
+            .get('/?search=r2-d2&format=json')
+            .reply(200, response);
+    })
+
+    it('deve buscar o r2d2 no formato correto', async () => {
+
+        const expected = [{ nome: 'R2-D2', peso: '96' }];
+        const nomeBase = 'r2-d2'
+
+        const result = await obterPessoas(nomeBase);
+        assert.deepEqual(result, expected);
+
+    });
+
+
+
+})
